d685416fcb714fbf1a19dc03c5a0b8aa097b0897
ntrdotapk <ntrdotapk@163.com>
2017-07-14 14:46:57 +0800

    maven

---
+++ learning/maven-use/src/test/java/ntrdotapk/AppTest.java
@@ -0,0 +1,16 @@
+package ntrdotapk;
+
+import org.junit.Test;
+
+/**
+ * 项目单元测试
+ *
+ * Created by ntrdotapk on 2017/7/14.
+ */
+public class AppTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
